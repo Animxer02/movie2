de903b31b89c38241475de64101f365510390726
Animxer02 <159162999+Animxer02@users.noreply.github.com>
2024-02-18 11:56:44 +0800

    Update moviedetail.js

--- moviedetail.js
+++ moviedetail.js
@@ -18,7 +18,7 @@ window.addEventListener("load", () => {
   loader.style.display = "none";
 });
 
-const myApiKey = "c73159ae3e0b40cf0883d7a33c0fea7f";
+const myApiKey = "9bf989f6bdfe3fc1ef2c288b9108d612";
 
 const searchMovies = document
   .querySelector(".search")
